2c38d0849aa0c14c646918278f050b4a6279e001
AmaniAlqarni <amanialqarni20@gmail.com>
2025-09-03 03:14:16 +0300

    Add assignments package

--- src/test/java/assignments/T4Day01.java
+++ src/test/java/assignments/T4Day01.java
@@ -1,4 +1,44 @@
 package assignments;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+
 public class T4Day01 {
+    public static void main(String[] args) throws InterruptedException {
+
+        // 1Open URL: https://www.google.com/
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+        // 2Maximize the window
+        driver.manage().window().maximize();
+
+        // 3Print the position and size of the page
+        Point position = driver.manage().window().getPosition();
+        Dimension size = driver.manage().window().getSize();
+        System.out.println("Maximized position: " + position);
+        System.out.println("Maximized size: " + size);
+
+        //4 Minimize the page
+        driver.manage().window().minimize();
+        Thread.sleep(5000);
+
+        // 5Maximize the page again
+        driver.manage().window().maximize();
+
+        // 6Print the position and dimensions of the page in maximized state
+        Point newPosition = driver.manage().window().getPosition();
+        Dimension newSize = driver.manage().window().getSize();
+        System.out.println("After maximize position: " + newPosition);
+        System.out.println("After maximize size: " + newSize);
+
+        // 7Make the page fullscreen
+        driver.manage().window().fullscreen();
+        Thread.sleep(3000);
+
+        // 8Close the Browser
+        driver.quit();
+    }
 }
