7f70552a252aa876a12c16f8373ba912f9de7177
AmaniAlqarni <amanialqarni20@gmail.com>
2025-09-10 12:36:31 +0300

    Add TestPackage package

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,23 @@
+package utilities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import java.time.Duration;
+
+public abstract class TestBase {
+    protected WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
